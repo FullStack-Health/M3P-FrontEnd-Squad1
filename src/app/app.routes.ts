49833317b5869f3266a9a2521c8628c4e5217a41
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-10-26 14:19:48 +0200

    release: organização pastas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,52 +1,61 @@
-import { Routes } from '@angular/router';
-import { SinginSignupPageComponent } from './singin-signup-page/singin-signup-page.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { HomeComponent } from './home/home.component';
-import { CadastroPacienteComponent } from './cadastro-paciente/cadastro-paciente.component';
-import { ExamesComponent } from './exames/exames.component';
-import { ConsultaComponent } from './consulta/consulta.component';
-import { ProntuariosComponent } from './prontuarios/prontuarios.component';
-import { ProntuarioPacienteComponent } from './prontuario-paciente/prontuario-paciente.component';
-import { authGuard } from './guards/auth.guard';
+import { Routes } from "@angular/router";
+import { SinginSignupPageComponent } from "./features/singin-signup-page/singin-signup-page.component";
+import { SignUpComponent } from "./features/sign-up/sign-up.component";
+import { CadastroPacienteComponent } from "./features/cadastro-paciente/cadastro-paciente.component";
+import { ConsultaComponent } from "./features/consulta/consulta.component";
+import { ProntuariosComponent } from "./features/prontuarios/prontuarios.component";
+import { ProntuarioPacienteComponent } from "./features/prontuario-paciente/prontuario-paciente.component";
+import { HomeComponent } from "./features/home/home.component";
+import { ExamesComponent } from "./features/exames/exames.component";
+import { authGuard } from "./core/guards/auth.guard";
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: "", redirectTo: "home", pathMatch: "full" },
     {
-        path: 'home', component: HomeComponent,
-        canActivate: [authGuard]
+        path: "home",
+        component: HomeComponent,
+        canActivate: [authGuard],
     },
-    { path: 'login', component: SinginSignupPageComponent },
-    { path: 'cadastro', component: SignUpComponent },
+    { path: "login", component: SinginSignupPageComponent },
+    { path: "cadastro", component: SignUpComponent },
     {
-        path: 'paciente', component: CadastroPacienteComponent,
-        canActivate: [authGuard]
-    },
-    {
-        path: 'paciente/edit/:id', component: CadastroPacienteComponent,
-        canActivate: [authGuard]
+        path: "paciente",
+        component: CadastroPacienteComponent,
+        canActivate: [authGuard],
     },
     {
-        path: 'exame', component: ExamesComponent,
-        canActivate: [authGuard]
+        path: "paciente/edit/:id",
+        component: CadastroPacienteComponent,
+        canActivate: [authGuard],
     },
     {
-        path: 'exame/:examId', component: ExamesComponent,
-        canActivate: [authGuard]
+        path: "exame",
+        component: ExamesComponent,
+        canActivate: [authGuard],
     },
     {
-        path: 'consulta', component: ConsultaComponent,
-        canActivate: [authGuard]
+        path: "exame/:examId",
+        component: ExamesComponent,
+        canActivate: [authGuard],
     },
     {
-        path: 'consulta/:consultaId', component: ConsultaComponent,
-        canActivate: [authGuard]
+        path: "consulta",
+        component: ConsultaComponent,
+        canActivate: [authGuard],
     },
     {
-        path: 'prontuarios', component: ProntuariosComponent,
-        canActivate: [authGuard]
+        path: "consulta/:consultaId",
+        component: ConsultaComponent,
+        canActivate: [authGuard],
     },
     {
-        path: 'prontuarios/:id', component: ProntuarioPacienteComponent,
-        canActivate: [authGuard]
-    }
+        path: "prontuarios",
+        component: ProntuariosComponent,
+        canActivate: [authGuard],
+    },
+    {
+        path: "prontuarios/:id",
+        component: ProntuarioPacienteComponent,
+        canActivate: [authGuard],
+    },
 ];
