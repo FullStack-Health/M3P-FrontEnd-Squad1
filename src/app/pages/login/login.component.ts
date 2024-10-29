07a62bdec5f1fae22153610d4d6c0e5e533458b2
Ricardo Viesant <152935023+viesant@users.noreply.github.com>
2024-10-29 11:40:57 +0100

    feat/esqueceu-senha: modal implementado

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -9,6 +9,7 @@ import {
 import { Router, RouterLink } from "@angular/router";
 import { AuthService } from "../../shared/services/auth.service";
 import Swal from "sweetalert2";
+import { UsuarioService } from "../../shared/services/usuario.service";
 
 @Component({
   selector: "app-login",
@@ -19,6 +20,7 @@ import Swal from "sweetalert2";
 })
 export class LoginComponent implements OnInit {
   private authService = inject(AuthService);
+  private usuarioService = inject(UsuarioService);
   private router = inject(Router);
   private formBuilder = inject(FormBuilder);
 
@@ -64,7 +66,65 @@ export class LoginComponent implements OnInit {
   }
 
   esqueciSenha() {
-    console.error("Não implementado ainda!");
-    // this.authService.esqueciSenha(this.loginForm);
+    Swal.fire({
+      title: "Redefinir Senha",
+      html: `
+        <input type="email" id="email" class="swal2-input" placeholder="Email">
+        <input type="password" id="newPassword" class="swal2-input" placeholder="Nova Senha">
+      `,
+      confirmButtonText: "Enviar",
+      focusConfirm: false,
+      showCancelButton: true,
+      cancelButtonText: "Cancelar",
+      preConfirm: () => {
+        const email = (
+          Swal.getPopup()?.querySelector("#email") as HTMLInputElement
+        ).value;
+        const newPassword = (
+          Swal.getPopup()?.querySelector("#newPassword") as HTMLInputElement
+        ).value;
+
+        if (!email || !newPassword) {
+          Swal.showValidationMessage("Por favor, preencha todos os campos");
+          return;
+        }
+
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(email)) {
+          Swal.showValidationMessage("Insira um e-mail válido");
+          return;
+        }
+
+        if (newPassword.length < 8) {
+          Swal.showValidationMessage(
+            "A senha deve conter ao menos 8 caracteres"
+          );
+        }
+
+        return { email, newPassword };
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const { email, newPassword } = result.value!;
+        this.usuarioService.redefinirSenha({ email, newPassword }).subscribe({
+          next: () => {
+            Swal.fire({
+              text: "Senha redefinida com sucesso!",
+              icon: "success",
+              confirmButtonColor: "#0A7B73",
+              confirmButtonText: "OK",
+            });
+          },
+          error: (error) => {
+            Swal.fire({
+              text: `Erro: ${error.message}`,
+              icon: "error",
+              confirmButtonColor: "#0A7B73",
+              confirmButtonText: "OK",
+            });
+          },
+        });
+      }
+    });
   }
 }
