91e1888589fce24db086750274a0b9f20d69214c
Brk-SirGio <124716729+Brk-SirGio@users.noreply.github.com>
2024-10-28 21:12:41 -0300

    integração do cadastro com a API

--- src/app/features/sign-up/sign-up.component.ts
+++ src/app/features/sign-up/sign-up.component.ts
@@ -9,6 +9,8 @@ import { CustomValidatorService } from "../../shared/services/custom-validator.s
 import { Router } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import Swal from "sweetalert2";
+import { ApiService } from "../../core/services/api.service";
+
 
 @Component({
   selector: "app-sign-up",
@@ -24,71 +26,81 @@ export class SignUpComponent {
       this.customValidatorService.validarNomeCompleto(),
     ]),
     email: new FormControl("", [Validators.required, Validators.email]),
-    password: new FormControl("", [Validators.minLength(8), Validators.required]),
+    password: new FormControl("", [
+      Validators.minLength(8),
+      Validators.required,
+    ]),
     confirmarPassword: new FormControl("", [
       Validators.minLength(8),
       Validators.required,
     ]),
     codigoUsuario: new FormControl(""),
-    role: new FormControl ("", [Validators.required]) 
+    role: new FormControl("", [Validators.required]),
   });
 
   constructor(
     private customValidatorService: CustomValidatorService,
-    private Router: Router
+    private router: Router,
+    private apiService: ApiService // Injetando o ApiService
   ) {}
 
   cadastrar() {
-    const listaUsers = localStorage.getItem("cadastroData");
-    const users = listaUsers ? JSON.parse(listaUsers) : [];
-
-    const existingUser = users.find(
-      (user: any) => user.email === this.form.value.email
-    );
-
-    if (existingUser) {
-      alert("User already exists.");
-    } else {
-      if (
-        this.form.valid &&
-        this.form.value.password === this.form.value.confirmarPassword
-      ) {
-        const userCode = Math.floor(1000 + Math.random() * 9000);
-        this.form.patchValue({ codigoUsuario: userCode.toString() });
-
-        users.push(this.form.value);
-        localStorage.setItem("cadastroData", JSON.stringify(users));
-
-        this.form.controls["name"].setValue("");
-        this.form.controls["email"].setValue("");
-        this.form.controls["role"].setValue("");
-        this.form.controls["password"].setValue("");
-        this.form.controls["confirmarPassword"].setValue("");
-
-        Swal.fire({
-          text: "Cadastro efetuado com sucesso!",
-          icon: "success",
-          confirmButtonColor: "#0A7B73",
-          confirmButtonText: "OK",
-        }).then((result) => {
-          if (this.Router.url === "/login") {
-            window.location.reload();
+    if (
+      this.form.valid &&
+      this.form.value.password === this.form.value.confirmarPassword
+    ) {
+      // Definindo o objeto de dados para enviar ao backend
+      const userData = {
+        email: this.form.value.email,
+        password: this.form.value.password,
+        role: this.form.value.role
+      };
+  
+      // Chamada ao método de cadastro da API
+      this.apiService.postData("api/usuarios/pre-registro", userData).subscribe(
+        (response) => {
+          // Sucesso: Exibir mensagem de confirmação e redirecionar
+          Swal.fire({
+            text: "Cadastro efetuado com sucesso!",
+            icon: "success",
+            confirmButtonColor: "#0A7B73",
+            confirmButtonText: "OK",
+          }).then(() => {
+            this.router.navigate(["/login"]);
+          });
+        },
+        (error) => {
+          if (error.status === 409) {
+            // Exibir mensagem específica para erro 409 (Email já cadastrado)
+            Swal.fire({
+              text: "Email já cadastrado. Por favor, tente outro.",
+              icon: "warning",
+              confirmButtonColor: "#0A7B73",
+              confirmButtonText: "OK",
+            });
           } else {
-            this.Router.navigate(["/login"]);
+            // Erro genérico
+            Swal.fire({
+              text: "Erro ao efetuar cadastro. Tente novamente.",
+              icon: "error",
+              confirmButtonColor: "#0A7B73",
+              confirmButtonText: "OK",
+            });
           }
-        });
-      } else {
-        Swal.fire({
-          text: "Formulário inválido",
-          icon: "error",
-          confirmButtonColor: "#0A7B73",
-          confirmButtonText: "OK",
-        });
-      }
+        }
+      );
+    } else {
+      Swal.fire({
+        text: "Formulário inválido ou senhas não coincidem.",
+        icon: "error",
+        confirmButtonColor: "#0A7B73",
+        confirmButtonText: "OK",
+      });
     }
   }
+  
 
   goToLogin() {
-    this.Router.navigate(["/login"]);
+    this.router.navigate(["/login"]);
   }
 }
