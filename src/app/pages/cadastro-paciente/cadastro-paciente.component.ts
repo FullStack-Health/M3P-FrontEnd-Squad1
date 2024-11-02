8774f0a17df9f7fbfce28424f5d4362d80f88773
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-11-02 02:06:52 -0300

    Extrutura forms referenciado ao html/ via cep buscando dados corretamente

--- src/app/pages/cadastro-paciente/cadastro-paciente.component.ts
+++ src/app/pages/cadastro-paciente/cadastro-paciente.component.ts
@@ -15,6 +15,7 @@ import Swal from "sweetalert2";
 import { NgxMaskDirective, NgxMaskPipe } from "ngx-mask";
 import { ViaCepService } from "../../shared/services/via-cep.service";
 
+
 @Component({
   selector: "app-cadastro-paciente",
   standalone: true,
@@ -60,45 +61,30 @@ export class CadastroPacienteComponent implements OnInit {
   ) {
     this.detectScreenSize();
     this.form = new FormGroup({
-      fullName: new FormControl("", [
-        Validators.required,
-        Validators.minLength(8),
-        Validators.maxLength(64),
-      ]),
+      fullName: new FormControl("", [Validators.required, Validators.minLength(8), Validators.maxLength(64)]),
       gender: new FormControl("", Validators.required),
       birthDate: new FormControl("", Validators.required),
-      cpf: new FormControl("", 
-        Validators.required,),
-      rg: new FormControl("", [
-        Validators.required,
-        Validators.maxLength(20),
-      ]),
+      cpf: new FormControl("", Validators.required),
+      rg: new FormControl("", [Validators.required, Validators.maxLength(20)]),
       rgIssuer: new FormControl("", Validators.required),
       maritalStatus: new FormControl("", Validators.required),
-      phone: new FormControl("", 
-        Validators.required,),
+      phone: new FormControl("", Validators.required),
       email: new FormControl("", [Validators.required, Validators.email]),
-      placeOfBirth: new FormControl("", [
-        Validators.required,
-        Validators.minLength(8),
-        Validators.maxLength(64),
-      ]),
-      emergencyContact: new FormControl("", 
-        Validators.required,),
+      placeOfBirth: new FormControl("", [Validators.required, Validators.minLength(8), Validators.maxLength(64)]),
+      emergencyContact: new FormControl("", Validators.required),
       allergies: new FormControl("", Validators.required),
       specificCare: new FormControl(""),
       healthInsurance: new FormControl(""),
       healthInsuranceNumber: new FormControl(""),
       healthInsuranceValidity: new FormControl(""),
-      zipCode: new FormControl("", 
-        Validators.required,),
+      zipCode: new FormControl("", Validators.required),
       state: new FormControl({ value: "", disabled: true }),
       city: new FormControl({ value: "", disabled: true }),
       street: new FormControl({ value: "", disabled: true }),
       number: new FormControl("", Validators.required),
       complement: new FormControl(""),
       neighborhood: new FormControl({ value: "", disabled: true }),
-      referencePoint: new FormControl(""),
+      referencePoint: new FormControl("")
     });
   }
 
@@ -114,10 +100,13 @@ export class CadastroPacienteComponent implements OnInit {
         });
       }
     });
-
+  
     this.form.get("zipCode")?.valueChanges.subscribe((cep) => {
-      if (cep && cep.length === 9) {
-        this.viaCepService.get(cep).subscribe((address) => {
+      console.log("CEP alterado:", cep); // Log para verificar o valor do CEP
+      const sanitizedCep = cep.replace("-", "");
+      if (sanitizedCep.length === 8) {
+        this.viaCepService.get(sanitizedCep).subscribe((address) => {
+          console.log("Endereço recebido:", address); // Log para verificar o endereço recebido
           this.form.patchValue({
             state: address.uf,
             city: address.localidade,
@@ -135,11 +124,19 @@ export class CadastroPacienteComponent implements OnInit {
       }
     });
   }
-
+  
   cadastrar() {
     if (this.form.valid) {
+      const pacienteData = {
+        ...this.form.value,
+        birthDate: this.formatDate(this.form.value.birthDate),
+        healthInsuranceValidity: this.formatDate(this.form.value.healthInsuranceValidity),
+        allergies: this.form.value.allergies.split(',').map((allergy: string) => allergy.trim()),
+        specificCare: this.form.value.specificCare.split(',').map((care: string) => care.trim())
+      };
+
       if (this.isEdit) {
-        this.pacienteService.updatePaciente(this.form.value).subscribe(() => {
+        this.pacienteService.updatePaciente(pacienteData).subscribe(() => {
           Swal.fire({
             text: "Cadastro atualizado com sucesso!",
             icon: "success",
@@ -149,7 +146,7 @@ export class CadastroPacienteComponent implements OnInit {
           this.router.navigate(["/home"]);
         });
       } else {
-        this.pacienteService.addPaciente(this.form.value).subscribe(() => {
+        this.pacienteService.addPaciente(pacienteData).subscribe(() => {
           Swal.fire({
             text: "Paciente cadastrado com sucesso!",
             icon: "success",
@@ -166,6 +163,11 @@ export class CadastroPacienteComponent implements OnInit {
         confirmButtonColor: "#0A7B73",
       });
     }
+  }
+
+  formatDate(date: string): string {
+    const [day, month, year] = date.split('-');
+    return `${year}-${month}-${day}`;
   }
 
   hasConsultasOrExams(patientId: string): boolean {
--- src/app/shared/services/via-cep.service.ts
+++ src/app/shared/services/via-cep.service.ts
@@ -1,19 +1,22 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, Observable, throwError } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ViaCepService {
-
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   get(cep: string): Observable<any> {
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
-    return this.http.get(url);
+    const sanitizedCep = cep.replace("-", "");
+    const url = `https://viacep.com.br/ws/${sanitizedCep}/json/`;
+    console.log("Chamando API ViaCep com URL:", url); // Adicione este log para verificar se a URL está correta
+    return this.http.get(url).pipe(
+      catchError((error) => {
+        console.error("Erro ao buscar CEP:", error);
+        return throwError(() => new Error("CEP inválido ou não encontrado."));
+      })
+    );
   }
-  
 }
